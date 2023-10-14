31144c21ee32ad608bd85ae6fce4e81b0d284c9a
ElJakes <jidejacobs1@gmail.com>
2023-10-14 01:29:17 +0100

    modify codes

--- data.js
+++ data.js
@@ -1,39 +1,44 @@
 const data = [
   {
     id: 1,
-    musicArt: "media/art-early-momo.jpg",
+    albumCover: "media/art-early-momo.jpg",
+    altText: "music art for a song by vector titled Early Momo",
     songTitle: "Early Momo",
     artiste: "Vector ft GoodGirl La",
-    song: "media/early-momo.mp3"
+    song: "media/early-momo.mp3",
   },
   {
     id: 2,
-    musicArt: "media/art-chale.jpg",
+    albumCover: "media/art-chale.jpg",
+    altText: "music art for a song by sho Madjozi titled Chale",
     songTitle: "Chale",
     artiste: "Sho Madjozi",
-    song: "media/chale.mp3"
+    song: "media/chale.mp3",
   },
   {
     id: 3,
-    musicArt: "media/art-empire.jpg",
+    albumCover: "media/art-empire.jpg",
+    altText: "music art for a song by Jay-Z titled Empire State of Mind",
     songTitle: "Empire State of Mind",
-    artiste: "Jay Z ft Alicia Keys",
-    song: "media/early-momo.mp3"
+    artiste: "Jay-Z ft Alicia Keys",
+    song: "media/early-momo.mp3",
   },
   {
     id: 4,
-    musicArt: "media/art-kota-the-friend.jpg",
+    albumCover: "media/art-kota-the-friend.jpg",
+    altText: "music art for a song by Kota The Friend titled Real Ones",
     songTitle: "Real Ones",
     artiste: "Kota The Friend",
-    song: "media/real-ones.mp3"
+    song: "media/real-ones.mp3",
   },
   {
     id: 5,
-    musicArt: "media/art-wet-dremz.jpg",
+    albumCover: "media/art-wet-dreamz.jpg",
+    altText: "music art for a song by J Cole titled Wet Dreamz",
     songTitle: "Wet Dreamz",
     artiste: "J Cole",
-    song: "media/wet-dreamz.mp3"
-  }
+    song: "media/wet-dreamz.mp3",
+  },
 ];
 
-export default data
+export default data;
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-import data from './data.js'
-console.log(data)
+import data from "./data.js";
+
+const musicArt = document.getElementById("music-art");
+const songTitle = document.getElementById("song-title");
+const artiste = document.getElementById("artiste");
 
 const progress = document.getElementById("progress");
 const playPauseIcon = document.getElementById("play-pause-icon");
 const controlIcon = document.getElementById("control-icon");
 const song = document.getElementById("song");
 
+const previousIcon = document.getElementById("previous-icon");
+const nextIcon = document.getElementById("next-icon");
+
+data.map((music) => {
+  musicArt.innerHTML = `<img class="music-art" src="${music.albumCover}" alt="${music.altText}">`;
+  songTitle.textContent = `${music.songTitle}`;
+  artiste.textContent = `${music.artiste}`;
+  song.innerHTML = `<source src="${music.song}" type="audio/mpeg">`;
+});
+
 song.onloadedmetadata = function () {
   progress.max = song.duration;
   progress.value = song.currentTime;
 };
 
-playPauseIcon.addEventListener("click", function() {
+playPauseIcon.addEventListener("click", function () {
   if (controlIcon.classList.contains("fa-pause")) {
     song.pause();
     controlIcon.classList.remove("fa-pause");
@@ -23,16 +36,23 @@ playPauseIcon.addEventListener("click", function() {
   }
 });
 
-// if (song.play()) {
-//   setInterval(() => {
-//     progress.value = song.currentTime;
-//   }, 500);
-// }
-
-
-progress.onchange = function() {
-    song.play();
-    song.currentTime = progress.value;
-    controlIcon.classList.add("fa-pause");
-    controlIcon.classList.remove("fa-play");
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
 }
+
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+};
+
+nextIcon.addEventListener("click", function () {
+  console.log("play next song");
+});
+
+previousIcon.addEventListener("click", function () {
+  console.log("play previous song");
+});
